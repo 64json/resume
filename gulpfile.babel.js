9684a0ea906f00d8dc85a15109bb95e71cdcddd5
Jason Park <parkjs814@gmail.com>
2019-09-30 16:25:24 +0800

    Fix FontAwesome not showing up in PDF

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -50,6 +50,7 @@ gulp.task('createPDF', () =>
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         await page.goto(`http://localhost:${port}/resume.html`, { waitUntil: 'networkidle2' });
+        await new Promise(resolve => setTimeout(resolve, 1000)); // TODO: need to find a better way to fix FontAwesome not showing up
         const buffer = await page.pdf({ printBackground: true });
         await browser.close();
         file.contents = buffer;
